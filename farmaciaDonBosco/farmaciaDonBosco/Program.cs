ed08480a7a8c6b2fa5518662a18c653230b87fde
JorgeMiraFlores <jorge.mira@oportunidades.org.sv>
2024-08-26 19:46:13 -0600

    changes

--- farmaciaDonBosco/farmaciaDonBosco/Program.cs
+++ farmaciaDonBosco/farmaciaDonBosco/Program.cs
@@ -18,7 +18,7 @@ namespace farmaciaDonBosco
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Productos());
+            Application.Run(new Login ());
             //Application.Run(new Login());
             //Application.Run(new Dashboard());
         }
